0b0d86e8fe3f1e89287a0ace738fa7e1e3dbafa3
Yaman Kahveci <yamooonz@gmail.com>
2020-12-09 21:36:11 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,23 @@
+  let lastRenderTime = 0
+  const SNAKE_SPEED = 2
+
+  function main(currentTime){
+    window.requestAnimationFrame(main) 
+    const secondsSinceLastRender = (currentTime - lastRenderTime)/1000
+     if ( secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+     lastRenderTime= currentTime
+
+     update()
+     draw()
+    }
+
+    window.requestAnimationFrame(main)
+
+    function update(){
+        updateSnake()
+    }
+
+    function draw (){
+        drawSnake(gamrBoard)
+    }
